625359629a8ba473f5da854523706b52da653413
Miki <62096691+miki5421@users.noreply.github.com>
2024-01-13 15:24:45 +0000

    fixed collect static

--- project/core/settings.py
+++ project/core/settings.py
@@ -87,8 +87,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "src") if not DEBUG else None
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "src"),) if DEBUG else None
+STATIC_ROOT = os.path.join(BASE_DIR, "src")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
