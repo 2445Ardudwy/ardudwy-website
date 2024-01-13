3ca7631c9a2abd674fb0d71fbc99798cd5d5a217
Miki <62096691+miki5421@users.noreply.github.com>
2024-01-13 17:44:03 +0000

    update to static and media files for docker and django

--- project/core/settings.py
+++ project/core/settings.py
@@ -87,11 +87,11 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "src")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'src')
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 GOV_NOTIFY_APIS = {
     'DEV_KEY': os.getenv('NOTIFY_DEV_KEY'),
